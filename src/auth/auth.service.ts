319927394e5c1397c1bd8947b05ae076a455a13c
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-04-11 14:09:39 -0700

    feat: Logica basica para crear usuarios en auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  create() {
-    return 'This action adds a new auth';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createAuthDto: CreateAuthDto) {
+    const usuarioExiste = await this.prisma.user.findUnique({
+      where: { usuario: createAuthDto.usuario },
+    });
+
+    if (usuarioExiste) throw new ConflictException('El usuario ya existe');
+
+    return this.prisma.user.create({
+      data: createAuthDto,
+    });
   }
 
   findAll() {
-    return `This action returns all auth`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `Esta accion retorna #${id} de autenticacion`;
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) throw new NotFoundException(`Usuario #${id} no encontrado`);
+
+    return user;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  async update(id: number, updateAuthDto: UpdateAuthDto) {
+    await this.findOne(id); // valida que exista
+
+    return this.prisma.user.update({
+      where: { id },
+      data: updateAuthDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async remove(id: number) {
+    await this.findOne(id); // valida que exista
+
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,33 @@
-export class CreateAuthDto {}
+import {
+  IsString,
+  IsBoolean,
+  IsDate,
+  IsInt,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateAuthDto {
+  @IsString()
+  usuario: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsBoolean()
+  activo: boolean;
+
+  @IsDate()
+  @Type(() => Date)
+  ultimo_login: Date;
+
+  @IsInt()
+  login_intentos: number;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsOptional()
+  bloqueado_hasta: Date;
+}
