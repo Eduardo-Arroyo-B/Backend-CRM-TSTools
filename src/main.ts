b119f352e7ebcf5fa1c0192b827f1b2b63dd240e
Eduardo Arroyo <aeduardoiran@gmail.com>
2026-04-25 12:39:15 -0700

    fix: se agrego el envio de cookies y ajustes dependiendo el ambiente, ademas de ajustes en la logica para cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(cookieParser());
+  app.set(cookieParser());
+
+  app.use('trust proxy', 1);
 
   app.enableCors({
     origin: [
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -30,7 +30,7 @@ export class AuthController {
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     });
 
     return { message: 'Login exitoso' };
