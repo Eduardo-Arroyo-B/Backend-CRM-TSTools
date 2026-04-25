cd58eb4d9069927780afa988b62f8fecc7ebe8c3
Eduardo Arroyo <aeduardoiran@gmail.com>
2026-04-25 13:20:52 -0700

    fix: se agrego duracion a la cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -31,6 +31,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      maxAge: 1000 * 60 * 60 * 24, // 1 dia en milisegundos
     });
 
     return { message: 'Login exitoso' };
