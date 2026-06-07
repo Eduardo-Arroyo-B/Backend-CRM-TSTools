ce8f94207b04a8cbadcc74670a32c4f66c3c1d3e
Eduardo Arroyo <aeduardoiran@gmail.com>
2026-06-07 12:16:54 -0700

    feat: cuando se inicia sesion por google se autocrea el nombre de la compañia, para poder mostrarlo

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -60,6 +60,12 @@ export class AuthService {
     if (!user) {
       const username = email.split('@')[0];
 
+      const tenant = await this.prisma.tenant.create({
+        data: {
+          nombre: `${username} Workspace`,
+        },
+      });
+
       user = await this.prisma.user.create({
         data: {
           usuario: username,
@@ -68,6 +74,8 @@ export class AuthService {
           googleId: googleUser.googleId,
           password: null,
           ultimo_login: new Date(),
+
+          tenantId: tenant.id,
         },
       });
     } else {
